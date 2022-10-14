891ff9c4d3821256ed09cf3d82442849f225d6b7
hajung00 <gkwjd719@naver.com>
2022-10-14 16:46:24 +0900

    FEAT: Router implement

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <h2>App.js</h2>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/new' element={<New />}></Route>
+          <Route path='/edit' element={<Edit />}></Route>
+          <Route path='/diary' element={<Diary />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
