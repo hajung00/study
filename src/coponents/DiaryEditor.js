09466c46fe6cedf7ef342ce5e5dd8cfc766e74bc
hajung00 <gkwjd719@naver.com>
2022-10-28 20:11:43 +0900

    FEAT: Diarypage implement

--- src/coponents/DiaryEditor.js
+++ src/coponents/DiaryEditor.js
@@ -4,38 +4,7 @@ import Mybutton from './Mybutton';
 import Myheader from './Myheader';
 import { DiaryDispatchContext } from '../App';
 import EmotionItem from './EmotionItem';
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
-
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + '/assets/emotion1.png',
-    emotion_descript: '완전 좋음',
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + '/assets/emotion2.png',
-    emotion_descript: '좋음',
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + '/assets/emotion3.png',
-    emotion_descript: '그럭저럭',
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + '/assets/emotion4.png',
-    emotion_descript: '나쁨',
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + '/assets/emotion5.png',
-    emotion_descript: '끔직함',
-  },
-];
+import { getStringDate, emotionList } from '../util/date';
 
 function DiaryEditor({ isEdit, originData }) {
   const navigate = useNavigate();
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import Mybutton from '../coponents/Mybutton';
+import Myheader from '../coponents/Myheader';
+import { DiaryStateContext } from './../App';
+import { getStringDate, emotionList } from './../util/date';
 
 function Diary() {
-  return <div>Diray</div>;
+  const { id } = useParams();
+  const nagative = useNavigate();
+  const diaryList = useContext(DiaryStateContext);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (diaryList.length > 1) {
+      const targetDiary = diaryList.find(
+        (item) => parseInt(item.id) === parseInt(id)
+      );
+      if (targetDiary) {
+        setData(targetDiary);
+      } else {
+        alert('없는 일기입니다.');
+        nagative('/', { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+
+  if (!data) {
+    return <div className='DiaryPage'>로딩중입니다...</div>;
+  } else {
+    const curEmotionData = emotionList.find(
+      (item) => parseInt(item.emotion_id) === parseInt(data.emotion)
+    );
+    console.log(curEmotionData);
+    return (
+      <div className='DiaryPage'>
+        <Myheader
+          headText={`${getStringDate(new Date(data.date))} 기록`}
+          leftChild={
+            <Mybutton text={'< 뒤로가기'} onClick={() => nagative(-1)} />
+          }
+          rightChild={
+            <Mybutton
+              text={'수정하기'}
+              onClick={() => nagative(`/edit/${data.id}`)}
+            />
+          }
+        />
+        <article>
+          <section>
+            <h4>오늘의 감정</h4>
+            <div
+              className={[
+                'diary_img_wrapper ',
+                `diary_img_wrapper_${data.emotion}`,
+              ].join('')}
+            >
+              <img src={curEmotionData.emotion_img} />
+              <div className='emotion_descript'>
+                {curEmotionData.emotion_descript}
+              </div>
+            </div>
+          </section>
+          <section>
+            <h4>오늘의 일기</h4>
+            <div className='diary_content-wrapper'>
+              <p>{data.content}</p>
+            </div>
+          </section>
+        </article>
+      </div>
+    );
+  }
 }
 
 export default Diary;
