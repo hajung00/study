0c80b43ecdd1dc8e7a8696129d12454979111428
hajung00 <gkwjd719@naver.com>
2023-05-22 21:34:15 +0900

    create: header component

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStringMode: true,
+  images: {
+    domains: ['lecture-1.vercel.app'],
+  },
+};
+
+module.exports = nextConfig;
