05d21b2f155bf6fa82bd347d52e85f93dfb8601a
hajung00 <gkwjd719@naver.com>
2023-08-22 23:30:19 +0900

    2023-08-22: webpack설정 & webpack-dev-server setting

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -1,15 +1,24 @@
-const path = require('path');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const webpack = require('webpack');
+import path from 'path';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import webpack, { Configuration as WebpackConfiguration } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
-const config = {
-  name: 'sleact',
+const config: Configuration = {
+  name: 'sleact', // webpack 설정 이름
   mode: isDevelopment ? 'development' : 'production',
-  devtool: isDevelopment ? 'hidden-source-map' : 'inline-source-map',
+  devtool: !isDevelopment ? 'hidden-source-map' : 'eval',
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'], // 바벨이 처리할 확장자 목록
+    // 소스를 검사하는건 ts검사기(tsconfig보고 검사), 자바스크립트로 바꿔주는 것은 webpack
+    // 그래서, tsconfig와 webpack에서 모두 설정해줘야함.
     alias: {
       '@hooks': path.resolve(__dirname, 'hooks'),
       '@components': path.resolve(__dirname, 'components'),
@@ -20,23 +29,24 @@ const config = {
     },
   },
   entry: {
-    app: './client',
+    app: './client', // build 대상 파일
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        options: {
+        test: /\.tsx?$/, // ts, tsx 파일을
+        loader: 'babel-loader', // 바벨 로더가 js로 변환
+        options: { // 바벨 설정
           presets: [
             [
               '@babel/preset-env',
               {
-                targets: { browsers: ['last 2 chrome versions'] },
+                targets: { browsers: ['IE 10'] },
                 debug: isDevelopment,
               },
             ],
             '@babel/preset-react',
+            '@babel/preset-typescript',
           ],
           env: {
             development: {
@@ -52,14 +62,22 @@ const config = {
       },
     ],
   },
-  plugins: [new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' })],
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      // eslint: {
+      //   files: "./src/**/*",
+      // },
+    }),
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
     publicPath: '/dist/',
   },
   devServer: {
-    historyApiFallback: true,
+    historyApiFallback: true, // react router
     port: 3090,
     devMiddleware: { publicPath: '/dist/' },
     static: { directory: path.resolve(__dirname) },
@@ -73,4 +91,4 @@ if (isDevelopment && config.plugins) {
 if (!isDevelopment && config.plugins) {
 }
 
-module.exports = config;
+export default config;
