587bea19610b59b72b2be2d3b6e128400c6d1214
hajung00 <gkwjd719@naver.com>
2023-08-23 23:29:18 +0900

    2023-08-23: 회원 가입 폼

--- client/layouts/App.tsx
+++ client/layouts/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import loadable from '@loadable/component';
+import { Redirect, Route, Switch } from 'react-router';
+
+const LogIn = loadable(()=> import('@pages/LogIn'))
+const SignUP = loadable(()=> import('@pages/SignUp'))
 
 const App = () => {
-  return <div>초기 세팅입니다.</div>;
+  return(
+    <Switch>
+      <Redirect exact path='/' to='/login'/>
+      <Route path='/login' component={LogIn}/>
+      <Route path='/signup' component={SignUP}/>
+    </Switch>
+  )
 };
 
 export default App;
