9995997f0bc0a000d822dbf095e0167a61e31c37
hajung00 <gkwjd719@naver.com>
2023-10-21 16:30:25 +0900

    2023-10-21: DM페이지 생성

--- client/pages/DirectMessage/index.tsx
+++ client/pages/DirectMessage/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Container, Header } from './styles';
+import { useParams } from 'react-router';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+import gravatar from 'gravatar';
+import ChatList from '@components/ChatList';
+import ChatBox from '@components/ChatBox';
 
 const DirectMessage = () => {
-  return <div>DirectMessage</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+
+  if (!userData || !myData) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.email} />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList />
+      <ChatBox chat="dd" />
+    </Container>
+  );
 };
 
 export default DirectMessage;
